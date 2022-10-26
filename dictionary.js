7eb597c1707edb4f0627ea496b82f27ff60f406d
nsblount111300 <nsblount111300@gmail.com>
2022-10-25 21:02:12 -0400

    idk

--- dictionary.js
+++ dictionary.js
@@ -1,15 +1,18 @@
 const options = {
   method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
 };
 
 function onSubmit() {
   const word = document.getElementById("word");
+  fetch(`http://localhost:8080/definitions/:${word}`, options)
+    .then((response) => response.json())
+    .then((response) => {
+      console.log(response);
+      document.getElementById("word").innerText = response.word;
+      // document.getElementById("definition").innerText = definition;
+    })
+    .catch((err) => console.error(err));
 }
-fetch(`http://localhost:8080/definitions/:word`)
-  .then((response) => response.json())
-  .then((response) => {
-    console.log(response);
-    document.getElementById("word").innerText = response[0].word;
-    document.getElementById("definition").innerText = response[0].definition;
-  })
-  .catch((err) => console.error(err));
