c94da524de1d47cd69d3bba7aafe172970e375d9
nsblount111300 <nsblount111300@gmail.com>
2022-10-27 01:50:33 -0400

    FIXXXX

--- dictionary.js
+++ dictionary.js
@@ -1,18 +1,23 @@
-const options = {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-};
+// const options = {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// };
 
 function onSubmit() {
-  const word = document.getElementById("word");
-  fetch(`http://localhost:8080/definitions/:${word}`, options)
+  const wordInput = document.getElementById("word").value.toLowerCase();
+  let input = wordInput.charAt(0).toUpperCase() + wordInput.slice(1);
+  fetch(`http://localhost:8080/definitions/${input}`)
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
-      document.getElementById("word").innerText = response.word;
-      // document.getElementById("definition").innerText = definition;
+      response.map((element) => {
+        const definition = document.getElementById("definition");
+        const paragraph = document.createElement("p");
+        paragraph.textContent = `${input} ${element.definition}`;
+        definition.appendChild(paragraph);
+      });
     })
     .catch((err) => console.error(err));
 }
