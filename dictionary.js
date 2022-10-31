8f2c9486ad06706ca3a7ecb18564195a5367e819
nsblount111300 <nsblount111300@gmail.com>
2022-10-31 00:53:05 -0400

    dictionary fix

--- dictionary.js
+++ dictionary.js
@@ -2,20 +2,19 @@ document.body.style.backgroundImage = `url('https://source.unsplash.com/1080x192
 
 function onSubmit() {
   const wordInput = document.getElementById("word").value.toLowerCase();
+  const definition = document.getElementById("definition");
+  definition.innerHTML = ``;
   let input = wordInput.charAt(0).toUpperCase() + wordInput.slice(1);
   fetch(`http://localhost:8080/definitions/${input}`)
     .then((response) => response.json())
     .then((response) => {
       console.log(response);
       response.map((element, index) => {
-        const definition = document.getElementById("definition");
         const paragraph = document.createElement("p");
-        const wordHead = (document.getElementById("word-header").innerText =
-          input);
-        paragraph.textContent = `[${index + 1}] ${input} - Definition: ${
-          element.values
-        }`;
-
+        document.getElementById("word-header").innerText = input;
+        paragraph.textContent = `[${index + 1}] ${input} (${
+          element.wordtype
+        })- Definition: ${element.definition}`;
         document.body.style.backgroundImage = `url('https://source.unsplash.com/1920x1080/?${input}')`;
         document.getElementById("def-container").style.display = "grid";
         definition.appendChild(paragraph);
